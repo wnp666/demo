2ae64ff7a92f4a92679b1cd143da3c39429d05cd
W杰杰杰 <3104353475@qq.com>
2026-04-09 22:18:09 +0800

    hello worlf.java的文件

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// 这是第一个Java程序，用于输出"Hello, World!"
+public class HelloWorld {
+    // main方法是程序的入口点
+    public static void main(String[] args) {
+        // 向控制台输出字符串
+        System.out.println("Hello, World!");
+    }
+}
