da67fac97d5e50d27244ecca377b24cee60c039e
W杰杰杰 <3104353475@qq.com>
2026-04-15 14:54:11 +0800

    feat: 完成算术运算符基础练习，删除无用Main.java

--- day02-code/src/com/wj/arithmeticoperator/ArithmeticOperator.java
+++ day02-code/src/com/wj/arithmeticoperator/ArithmeticOperator.java
@@ -14,7 +14,7 @@ public class ArithmeticOperator {
         //在代码中，如果有小数参与计算，结果有可能不精确的。
         //为什么呢？
         //暂时只要知道这个结论就可以了。
-        //具体的原因，我们到了JavaSE的时候，会再详细的讲解。
+        //具体的原因，我们到了JavaSE的时候，会再详细的讲解
         System.out.println(1.1 + 1.1);//2.2
         System.out.println(1.1 + 1.01);//2.1100000000000003
         System.out.println(1.1 - 1.01);
